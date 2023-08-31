6b75f9256d05e3eb6e9739bdf5152aabed6b7592
rabbani4443 <rabbani07r@gmail.com>
2023-08-31 08:39:35 +0600

    finished the project

--- js/index.js
+++ js/index.js
@@ -30,28 +30,30 @@ const handelLoadNews = async(categoryId) =>{
         <div  class="card bg-gray-200 shadow-xl p-5">
         <figure class="pb-5"><img src=${showTheNews.thumbnail_url} alt="Shoes" class = "w-full" /></figure>
         <div class="">
-            <div class="flex ">
-                <h2 class="card-title">Welcome to my website ! Welcome to my website ! </h2>
-                <div class="whitespace-nowrap">
-                    <button class=" bg-rose-600 text-white rounded-lg px-4 py-2 ">Excellent</button>
+            <div class="flex gap-2">
+                <h2 class="card-title">${showTheNews.title.slice(0, 40)}</h2>
+                <div class="whitespace-nowrap ">
+                    <button class=" bg-rose-600 text-white rounded-lg px-4 py-2 ">${showTheNews.rating.badge}</button>
                 </div>
                   
             </div>
-          <p class="py-3">If a dog chews shoes whose shoes does he choose? If a dog chews shoes whose shoes does he choose?If a dog chews shoes whose shoes does he choose?</p>
+          <p class="py-3" >${showTheNews.details.slice(0,150)}</p>
+          <p class="pb-4 font-medium ">Total View : ${showTheNews.total_view?showTheNews.total_view: "No Data"}</p>
+
         </div>
 
         <div class="flex items-center justify-between ">   
             <div class="flex space-x-3">
                 <div class="mask mask-squircle w-12 h-12 ">
-                    <img src="https://daisyui.com/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+                    <img src=${showTheNews.author.img} alt="Avatar Tailwind CSS Component" />
                   </div>
                 <div>
-                  <div class="font-bold">Hart Hagerty</div>
-                  <div class="text-sm opacity-50">2022-08-24 17:27:34</div>
+                  <div class="font-bold">${showTheNews.author.name? showTheNews.author.name:"No Data"}</div>
+                  <div class="text-sm opacity-50">${showTheNews.author.published_date}</div>
                 </div>
             </div>
                 <div class="whitespace-nowrap">
-                    <button class=" bg-gray-900 text-white rounded-lg px-4 py-2 ">Detail</button>
+                    <button onclick="handelModal('${showTheNews._id}')" class="bg-gray-900 text-white rounded-lg px-4 py-2">Detail</button>
               </div>
           </div>
       </div> 
@@ -61,4 +63,41 @@ const handelLoadNews = async(categoryId) =>{
     });
 };
 
-allNewsCategory()
+const handelModal =async(modalId)=>{
+    // console.log(modalId);
+    const responseLink = await fetch(`https://openapi.programming-hero.com/api/news/${modalId}`);
+    const response = await responseLink.json();
+    const data = await response.data[0];
+    console.log(data);
+
+    const showTheModalId = document.getElementById('show-the-modal');
+    const div = document.createElement('div');
+    div.innerHTML=`
+    <dialog id="my_modal_1" class="modal">
+    
+    <form method="dialog" class="modal-box shadow-xl">
+    <div  class=" p-5">
+    <figure class="pb-5"><img src=${data.thumbnail_url} alt="Shoes" class = "w-full" /></figure>
+    <div class="">
+        <div class="flex gap-2">
+            <h2 class="card-title">${data.title}</h2>
+        </div>
+      <p class="py-3" >${data.details}</p>
+      <p class="pb-4 font-medium ">Total View : ${data.total_view?data.total_view: "No Data"}</p>
+    </div>
+  </div> 
+
+      
+      <div class="modal-action">
+        <button class="btn">Close</button>
+      </div>
+    </form>
+  </dialog>
+    `;
+    showTheModalId.appendChild(div);
+    const showModal = document.getElementById('my_modal_1');
+    showModal.showModal();
+};
+
+allNewsCategory();
+handelLoadNews(categoryId="01");
